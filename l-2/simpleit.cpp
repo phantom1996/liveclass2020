0785ce28730e15a000f2ed7d36d9396914a9e8a3
sharad nailwal <sharadnailwal96@gmail.com>
2020-06-04 00:03:34 +0530

    lecture 2

---
+++ l-2/simpleit.cpp
@@ -0,0 +1,29 @@
+// simpleit
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//declare of variale
+	int p,r,t;
+	// int p;
+	// int r;
+	// int t;
+	float si;
+
+	//user iput
+	// cin >> p;
+	// cin >> r;
+	// cin >> t;
+	
+	cin >>p>>r>>t;
+
+	si = (p*r*t)/100.0;
+
+	//prit
+	cout << si;
+	cout <<'\n';
+	cout<< "hello locks";
+
+	return 0;
+}
